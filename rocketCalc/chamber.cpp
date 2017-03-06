888bf77e6afa768cd7e7d2efef044b9d24e33144
JackWagner <jack.wagner5299@gmail.com>
2017-03-06 15:44:13 -0800

    Update chamber.cpp

--- rocketCalc/chamber.cpp
+++ rocketCalc/chamber.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <stdlib>
 #include <cmath>
 
-int main() {
+int main(int argc , char  *argv[]) {
 
+  //meters
+  double ChamberDiameter = strtod(argv[1],NULL);
+  //psi
+  double ChamberPressure = strtod(argv[2],NULL);
+  
   return 0;
 
 }
@@ -39,8 +45,17 @@ double nozzleTemperature() {
   return chamberTemperature()*( 1 / ( 1 + ( gamma() - 1) / 2 ) );
 }
 
+//ChamberPressure input is in psi, converted to pascals
 double nozzlePressure() {
-  return chamberPressure()*pow((1 + (gamma() - 1) / 2 ), -1 * gamma()/(gamma()-1));
+  return 6894.75729*(ChamberPressure*pow((1 + (gamma() - 1) / 2 ), -1 * gamma()/(gamma()-1)));
+}
+
+double chamberTemperature() {
+  return 2.321*pow(10,-8)*pow(ChamberPressure,3) - 1.955*pow(10,-4)*pow(ChamberPressure,2) + 0.575*ChamberPressure + 3027.817;
+}
+
+double gamma() {
+ return -6.988*pow(10,-13)*pow(ChamberPressure,3) + 6.032*pow(10,-9)*pow(ChamberPressure,2) - 1.823*pow(10,-5)*(ChamberPressure)+ 1.215; 
 }
 
 
