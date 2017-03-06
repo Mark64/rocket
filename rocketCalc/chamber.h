84a800985c67b16a32710f2240748d54e0ee5c60
JackWagner <jack.wagner5299@gmail.com>
2017-03-06 15:45:03 -0800

    Update chamber.h

--- rocketCalc/chamber.h
+++ rocketCalc/chamber.h
@@ -15,10 +15,22 @@ double nozzleThroatCrossArea();
 // returns the diameter of the nozzle throat in units of meters
 double nozzleThroatDiameter();
 
+// returns the cross area of the chamber in units of meters
+double chamberCrossArea();
+
+// returns the diameter of the chamber in units of meters
+double chamberDiameter();
+
 // returns the temperature of the exhaust at the nozzle in units of Kelvin
 double nozzleTemperature();
 
 // returns the pressure of the exhaust at the nozzle in units of Pascals
 double nozzlePressure();
+
+// returns the temperature of the chamber of the nozzle 
+double chamberTemperature();
+
+// returns the specific heat ratio of products
+double gamma();
 
 
