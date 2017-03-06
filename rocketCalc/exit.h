302def3aeb5bff0ec1cf306a339973be1f95822c
Mark Hill <markleehill@gmail.com>
2017-03-06 07:23:04 -0800

    started headers for chamber and exit calculations

---
+++ rocketCalc/exit.h
@@ -0,0 +1,19 @@
+// Contains code dealing with calculations for the exit area of the thruster
+//
+
+#include<iostream>
+
+
+// returns the area of the exit space for the thruster exhaust in units of meters^2
+double exitArea();
+
+// returns the diameter of the exit in units of meters
+double exitDiameter();
+
+// returns the velocity of the exiting gas in units of meters/second
+double exitGasVelocity();
+
+// returns the mean temperature for the exiting exhaust in degrees kelvin
+double exitGasTemperature();
+
+
