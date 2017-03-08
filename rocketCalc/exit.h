2700ac1b2cb475dc8b73ac0bd5ff931d7085c61f
JackWagner <jack.wagner5299@gmail.com>
2017-03-08 08:37:50 -0800

    More declarations for exit functions

--- rocketCalc/exit.h
+++ rocketCalc/exit.h
@@ -10,10 +10,19 @@ double exitArea();
 // returns the diameter of the exit in units of meters
 double exitDiameter();
 
+// returns the mach number of the exhaust at the exit
+double exitMach();
+
 // returns the velocity of the exiting gas in units of meters/second
 double exitGasVelocity();
 
 // returns the mean temperature for the exiting exhaust in degrees kelvin
 double exitGasTemperature();
+
+// returns the pressure at the exit in Pascals
+double exitPressure();
+
+// returns the ratio between exit cross-sectional area and nozzle throat cross-sectional area
+double expansionRatio();
 
 
